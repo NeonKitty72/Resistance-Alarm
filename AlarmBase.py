57f20634c46d93aab3d08ee6a03a341312c1d941
BigVixie <bigvixie@outlook.com>
2020-08-09 21:58:54 -0500

    oh shit I did everything - successful alerts when someone tweets!

--- AlarmBase.py
+++ AlarmBase.py
@@ -1,13 +1,21 @@
 # Copyright 2020 NeonKitty Industries
 # JK this is GNU GPL, use as you feel fit to
-import json
 import sys
+import json
+import threading
 from twython import Twython
 
 #CONSTANTS
 KEYS_FILEPATH = "secrets/AppKeys.json"
 APP_KEY_NAME= "APP_KEY"
 APP_SECRET_NAME = "APP_SECRET"
+FURSQUARED_USER_ID = "1327655468"
+
+print("----------------------------------------------------------")
+print("Resistance Alarm v0.1")
+print("Exit with Ctrl+C (or whatever break is on your cmd)")
+print("")
+print("initalizing...")
 
 #load up app keys
 try:
@@ -27,13 +35,58 @@ if keysJson[APP_SECRET_NAME] == "<APP_SECRET_HERE>":
 if error:
     sys.exit()
 
+print("loading app keys... good.")
+
 #get twitter oauth key
 appKey = keysJson[APP_KEY_NAME]
 appSecret = keysJson[APP_SECRET_NAME]
 twitter = Twython(appKey, appSecret, oauth_version=2)
 accessToken = twitter.obtain_access_token()
+print("getting twitter access token... good.")
 
 twitter = Twython(appKey, access_token=accessToken)
+appKey = ""
+appSecret = ""
+accessToken = ""
+print("clearing sensitive data... good.")
 
 #search for fursquared tweets
-print(twitter.get_user_timeline(user_id="2960430183"))
+#get the current tweet's id to know when tweets are actually new
+maxId = 0
+tweets = twitter.get_user_timeline(user_id=FURSQUARED_USER_ID,count=1)
+tweet = tweets[0]
+maxId = tweet["id"]
+print("initializing twitter link... good.")
+print("")
+print("Currently alerting for tweets/replies/rt's from user ID {}".format(FURSQUARED_USER_ID))
+print("last tweet occurred at {}".format(tweet["created_at"]))
+print("----------------------------------------------------------")
+
+# ------------------------------
+# functions
+# ------------------------------
+#AAAAAAAAAAAAAAAAAHHHHHHHHHHH
+def yell():
+    print("NEW TWEET NEW TWEET")
+
+#Every second, fetch the last two tweets. If any of them are new, yell.
+def huntForNewTweets():
+    global maxId
+    newTweet = False
+    tweets = twitter.get_user_timeline(user_id=FURSQUARED_USER_ID,count=1,since_id=maxId)
+    for tweet in tweets:
+        if tweet["id"] > maxId:
+            newTweet = True
+
+    if newTweet:
+        yell()
+        maxId = tweet["id"]
+    else:
+        print("nothing")
+
+    threading.Timer(1.5, huntForNewTweets).start()
+
+
+
+
+huntForNewTweets()
--- README.md
+++ README.md
@@ -6,11 +6,17 @@ An alarm for when the FurSquared twitter account tweets and the CyberSkunk resis
 2. [Create a Twitter App](https://apps.twitter.com/) and get the consumer key and consumer secret
 3. Copy the AppKeys.json file into the secrets folder. Edit it, replacing <APP_KEY_HERE> with your twitter app/API key and <APP_SECRET_HERE> with your twitter app/API secret. (Don't commit these, they're your secrets, not mine!)
 
+## References
+
+* [find a twitter's user id](https://tweeterid.com/)
+* [Twython Docs](https://twython.readthedocs.io/en/latest/index.html)
+* [GET user timeline twitter doc](https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline)
+
 ## Shit to do
 
 - [x] [get retrowave blastin'](https://open.spotify.com/playlist/2NvF2wLjCrwydXdc041LwC?si=OULzS2tvT4uOiunecLwHOQ)
-- [ ] get connecting to twitter
-- [ ] get a return when an account tweets
+- [x] get connecting to twitter
+- [x] get a return when an account tweets
 - [ ] get light blinking
 - [ ] get a cool alarm sound
 
